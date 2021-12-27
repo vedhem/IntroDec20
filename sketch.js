ded2dcad0d0cd8fbeeb2acc1e209820cc2e10822
vedhem <96457962+vedhem@users.noreply.github.com>
2021-12-27 11:35:33 -0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,30 +1,40 @@
-var cartoon;
+
 function setup() {
   createCanvas(400,400);
-  cartoon = createSprite(200, 200, 3, 3);
-  cartoon.addImage(cartoonImage);
-}
+  background(51);
+  box = createSprite(200,200,30,30);
 
-function preload () {
-  cartoonImage = loadImage("cartoon.jpg");
 }
 
 function draw() 
 {
-  background(30);
-  drawSprites();
-  if (keyIsDown(UP_ARROW)) {
-    cartoon.position.y = cartoon.position.y - 5;
-  }
-  if (keyIsDown(DOWN_ARROW)) {
-    cartoon.position.y = cartoon.position.y +5;
-  }
-  if (keyIsDown(LEFT_ARROW)) {
-    cartoon.position.x = cartoon.position.x - 5;
-  }
-  if (keyIsDown(RIGHT_ARROW)) {
-    cartoon.position.x = cartoon.position.x + 5;
-  }
-}
 
+  // write the code to change background color 
+  if (keyIsDown(RIGHT_ARROW)) {
+    background("red");
+  }
+  
+  
+
+  if (keyIsDown(LEFT_ARROW)) 
+  {
+    background("blue");
+    
+  }
+ 
+    if (keyIsDown(UP_ARROW)) 
+  {
+    background("yellow");
+   
+  }
+
+  if (keyIsDown(DOWN_ARROW)) 
+  {
+    background("green");
+  }
+
+
+  
+  drawSprites();
+}
 
